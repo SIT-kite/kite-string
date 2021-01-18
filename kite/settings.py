1de2f6495730f1999a1392e1950e8f8e60acdd14
sunnysab <sunnysab@yeah.net>
2021-01-18 11:25:44 +0800

    添加 scrapy-redis 支持.
    1. 使用 sracpy-redis 库实现分布式 & Url 去重
    2. 添加配置文件 settings.toml(示例: settings.toml.example)，以填写 redis 地址

--- kite/settings.py
+++ kite/settings.py
@@ -66,6 +66,7 @@ DEFAULT_REQUEST_HEADERS = {
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
     'kite.pipelines.FilePipeline': 300,
+    'scrapy_redis.pipelines.RedisPipeline': 400
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
@@ -88,3 +89,73 @@ AUTOTHROTTLE_TARGET_CONCURRENCY = 3.0
 # HTTPCACHE_DIR = 'httpcache'
 # HTTPCACHE_IGNORE_HTTP_CODES = []
 # HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+"""
+Scrapy-redis
+"""
+# Enables scheduling storing requests queue in redis.
+SCHEDULER = "scrapy_redis.scheduler.Scheduler"
+
+# Ensure all spiders share same duplicates filter through redis.
+DUPEFILTER_CLASS = "scrapy_redis.dupefilter.RFPDupeFilter"
+
+# Default requests serializer is pickle, but it can be changed to any module
+# with loads and dumps functions. Note that pickle is not compatible between
+# python versions.
+# Caveat: In python 3.x, the serializer must return strings keys and support
+# bytes as values. Because of this reason the json or msgpack module will not
+# work by default. In python 2.x there is no such issue and you can use
+# 'json' or 'msgpack' as serializers.
+# SCHEDULER_SERIALIZER = "scrapy_redis.picklecompat"
+
+# Don't cleanup redis queues, allows to pause/resume crawls.
+# SCHEDULER_PERSIST = True
+
+# Schedule requests using a priority queue. (default)
+# SCHEDULER_QUEUE_CLASS = 'scrapy_redis.queue.PriorityQueue'
+
+# Alternative queues.
+# SCHEDULER_QUEUE_CLASS = 'scrapy_redis.queue.FifoQueue'
+# SCHEDULER_QUEUE_CLASS = 'scrapy_redis.queue.LifoQueue'
+
+# Max idle time to prevent the spider from being closed when distributed crawling.
+# This only works if queue class is SpiderQueue or SpiderStack,
+# and may also block the same time when your spider start at the first time (because the queue is empty).
+# SCHEDULER_IDLE_BEFORE_CLOSE = 10
+
+# The item pipeline serializes and stores the items in this redis key.
+# REDIS_ITEMS_KEY = '%(spider)s:items'
+
+# The items serializer is by default ScrapyJSONEncoder. You can use any
+# importable path to a callable object.
+# REDIS_ITEMS_SERIALIZER = 'json.dumps'
+
+# Specify the host and port to use when connecting to Redis (optional).
+# REDIS_HOST = 'localhost'
+# REDIS_PORT = 6379
+
+# Specify the full Redis URL for connecting (optional).
+# If set, this takes precedence over the REDIS_HOST and REDIS_PORT settings.
+# Read from `settings.toml`
+import toml
+
+REDIS_URL = toml.load(open('settings.toml', encoding='utf-8'))['redis']['connection_string']
+
+# Custom redis client parameters (i.e.: socket timeout, etc.)
+REDIS_PARAMS = {}
+# Use custom redis client class.
+# REDIS_PARAMS['redis_cls'] = 'kite-string.RedisClient'
+
+# If True, it uses redis' ``SPOP`` operation. You have to use the ``SADD``
+# command to add URLs to the redis queue. This could be useful if you
+# want to avoid duplicates in your start urls list and the order of
+# processing does not matter.
+# REDIS_START_URLS_AS_SET = False
+
+# If True, it uses redis ``zrevrange`` and ``zremrangebyrank`` operation. You have to use the ``zadd``
+# command to add URLS and Scores to redis queue. This could be useful if you
+# want to use priority and avoid duplicates in your start urls list.
+# REDIS_START_URLS_AS_ZSET = False
+
+# Default start urls key for RedisSpider and RedisCrawlSpider.
+# REDIS_START_URLS_KEY = '%(name)s:start_urls'
