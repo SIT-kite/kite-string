382af35770e905b5172ddaea6b5083ca5314476c
sunnysab <sunnysab@yeah.net>
2021-04-06 19:35:21 +0800

    更新 document-parser.py, 添加 requirements.txt，重构文档文本提取和图片转换脚本

--- tools/document-parser.py
+++ tools/document-parser.py
@@ -1,140 +1,153 @@
-# Lib: libreoffice
-# Description: transform word into pdf
-# Installation: go to  https://www.cnblogs.com/ruozhu/p/11190195.html
-# you must make sure line 96 is same to your libreoffice's version
+# -*- coding: utf-8 -*-
+# @Time    : 2021/2
+# @Author  : wanfengcxz, sunnysab
+# @File    : document-parser.py
+# @Description : Use pdfminer: pdf -> text
+#             Use libreoffice: doc, docx, .. -> pdf
+#             Use
+# @Note    : The module will convert all documents into pdf format, and then use pdfminer to get text and its page num.
+#            Installation reference: https://www.cnblogs.com/ruozhu/p/11190195.html
+#            You must make sure the version of the libreoffice in the code is correct.
 
 import os
-import os.path
-
-# Lib: pymupdf
-# Description: pdf to png
-# Installation: pip install pymupdf
-import fitz
-from pdfminer.converter import PDFPageAggregator
-from pdfminer.layout import LTTextBoxHorizontal, LAParams
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.pdfinterp import PDFTextExtractionNotAllowed
-# Lib: pdfminer3k
-# Description: pdf content extract
-# Installation: pip install pdfminer3k
-from pdfminer.pdfparser import PDFParser, PDFDocument
+from typing import List, Tuple
 
 
-class DocExtract:
-    def __init__(self, doc_path=""):
-        # protected
-        self._doc_path = doc_path
-        # set doc path
+def split_file_name(file_name: str) -> (str, str):
+    """ Split a file name into file name and the extension. """
+    r = file_name.rfind('.')
+    if r != -1:
+        return file_name[:r], file_name[r + 1:]
+    else:
+        return file_name, ''
 
-    def load_path(self, doc_path):
-        self._doc_path = doc_path
+
+class DocExtractor:
+    """
+    Base class as a document extractor.
+    """
+
+    def __init__(self, path: str = ''):
+        self._path = path
 
 
-class PdfExtract(DocExtract):
-    def __init__(self, pdf_path=""):
-        super().__init__(pdf_path)
+class PdfExtractor(DocExtractor):
+    """
+    A text extractor for pdf files.
+    """
 
-    def convert2png(self, png_path=""):
-        pdfDoc = fitz.open(self._doc_path)
-        file_name = os.path.split(self._doc_path)[1]
-        file_name = file_name.split('.')[0]
-        if png_path == "":
-            png_path = './' + file_name
-        else:
-            png_path = png_path + '/' + file_name
+    def __init__(self, path: str = ''):
+        super().__init__(path)
 
-        if not os.path.exists(png_path):
-            os.makedirs(png_path)
-        for pg in range(pdfDoc.pageCount):
-            page = pdfDoc[pg]
-            rotate = int(0)
-            # 每个尺寸的缩放系数为1.3，这将为我们生成分辨率提高2.6的图像。
-            # 此处若是不做设置，默认图片大小为：792X612, dpi=72
-            zoom_x = 1.33333333  # (1.33333333-->1056x816)   (2-->1584x1224)
-            zoom_y = 1.33333333
-            mat = fitz.Matrix(zoom_x, zoom_y).preRotate(rotate)
-            pix = page.getPixmap(matrix=mat, alpha=False)
-            # extract file name          
-            pix.writePNG(png_path + '/' + file_name + '_%s.png' % pg)
-        print("Successfully save png to folder " + png_path)
+    def page2png(self, output_directory: str = None) -> List[str]:
+        """ Convert pdf pages into png files. Return the png file list. """
 
-    def show_path(self):
-        return self._doc_path
+        if not output_directory:
+            name, _ = split_file_name(self._path)
+            output_directory = f'{name}'
+            if not os.path.exists(output_directory):
+                os.mkdir(output_directory)
 
-    def pdf_extract(self):
-        return self._prase()
+        from wand.image import Image
+        pdf2img_obj = Image(filename=self._path, resolution=300)
+        png_objs = pdf2img_obj.convert('png')  # Convert Pdf file into png sequence.
 
-    def _prase(self):
-        fp = open(self._doc_path, 'rb')
-        parser = PDFParser(fp)
-        doc = PDFDocument()
-        parser.set_document(doc)
-        doc.set_parser(parser)
-        doc.initialize()
-        if not doc.is_extractable:
-            raise PDFTextExtractionNotAllowed
-        else:
+        output_files = []
+        # Write the sequence to file.
+        for index, img in enumerate(png_objs.sequence):
+            current_page = Image(image=img)
+            file_name = f'{output_directory}/{index}.png'
+
+            with open(file_name, 'wb') as out_image:
+                out_image.write(current_page.make_blob('png'))
+            output_files.append(file_name)
+
+        return output_files
+        # End of function page2png.
+
+    def text(self) -> List[Tuple[int, str]]:
+        """ Convert pdf pages into a list of text strings. """
+
+        from pdfminer.converter import PDFPageAggregator
+        from pdfminer.layout import LTTextBoxHorizontal, LAParams
+        from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+        from pdfminer.pdfparser import PDFParser, PDFDocument
+
+        with open(self._path, 'rb') as pdf_file:
+            doc = PDFDocument()
+
+            parser = PDFParser(pdf_file)
+            parser.set_document(doc)
+            doc.set_parser(parser)
+            doc.initialize()
+            if not doc.is_extractable:
+                raise Exception('The Pdf text extraction is not allowed when procesing ' + self._path)
+
             rsrcmgr = PDFResourceManager()
             laparams = LAParams()
             device = PDFPageAggregator(rsrcmgr, laparams=laparams)
             interpreter = PDFPageInterpreter(rsrcmgr, device)
-            # all content
-            content = []
-            for page in doc.get_pages():
+
+            result = []
+            for index, page in enumerate(doc.get_pages()):
                 interpreter.process_page(page)
                 layout = device.get_result()
-                # content in one page
-                content_single_page = ""
-                for x in layout:
-                    if (isinstance(x, LTTextBoxHorizontal)):
-                        content_single_page += x.get_text().strip()
-                content.append(content_single_page)
-        return content
+
+                current_page_text = ''.join([x.get_text().strip() for x in layout
+                                             if isinstance(x, LTTextBoxHorizontal)])
+                result.append((index, current_page_text))
+
+        return result
 
 
-class WordExtract(DocExtract):
-    _is2pdf = False
-    _pdfExtract = PdfExtract()
-    _pdf_out_path = os.getcwd() + '/tmp_pdf'
+class WordExtractor(DocExtractor):
+    """ A text extractor for doc, docx and other document files. """
 
-    def __init__(self, word_path=""):
+    def __init__(self, word_path=''):
         super().__init__(word_path)
 
     def show_path(self):
-        return self._doc_path
+        return self._path
 
-    def _word2pdf_linux(self):
-        cmd = 'libreoffice7.1 --headless --convert-to pdf ' + self._doc_path + ' --outdir ' + self._pdf_out_path
-        print(cmd)
-        res = os.system(cmd)
-        if res is not 0:
-            print("convert " + self._doc_path + "to pdf failed")
-        else:
-            self._is2pdf = True
+    @staticmethod
+    def word2pdf(word_file: str, out_pdf_file: str):
+        """ Convert word document to pdf file by libreoffice. """
 
-    def word_extract(self):
-        return self._prase()
+        import subprocess
+        command = f'libreoffice7.1 --headless --convert-to pdf {word_file} --outdir {out_pdf_file}'
+        subprocess.run(command, shell=True)
 
-    def convert2png(self, png_path=""):
-        if not self._is2pdf:
-            self._word2pdf_linux()
-        file_name = os.path.split(self._doc_path)[1].split('.')[0]
-        self._pdfExtract.load_path(self._pdf_out_path + '/' + file_name + '.pdf')
-        self._pdfExtract.convert2png(png_path)
+    def convert_to_pdf(self, out_pdf_file: str):
+        self.word2pdf(self._path, out_pdf_file)
 
-    def _prase(self):
-        if not self._is2pdf:
-            self._word2pdf_linux()
-        file_name = os.path.split(self._doc_path)[1].split('.')[0]
-        self._pdfExtract.load_path(self._pdf_out_path + '/' + file_name + '.pdf')
-        return self._pdfExtract.pdf_extract()
+    def convert2png(self, output_directory: str = None):
+        """ Convert word document to png files. """
+
+        # Firstly, convert doc file into pdf.
+        _pdf_extractor = PdfExtractor(self._path)
+        # Then, convert pdf to png files.
+        # The file name rule is set by PdfExtractor
+        _pdf_extractor.page2png(output_directory)
+
+    def text(self) -> List[Tuple[int, str]]:
+        """ Convert word document pages into a list of text strings. """
+
+        # The method convert word document to pdf, and then use pdf extractor to read text.
+        # It's not appropriate to read from word document directly, because it's hard to read the text with page number 
+        # from the docx libraries in Python. 
+        file_title, _ = split_file_name(self._path)
+        file_name = file_title + '.pdf'
+
+        self.convert_to_pdf(file_name)
+        _pdf_extractor = PdfExtractor(file_name)
+        return _pdf_extractor.text()
 
 
-wordExtract = WordExtract("./上海应用技术大学关于开展2019年学习标兵、学习型寝室的评选通知.doc")
-wordExtract.convert2png()
-print(wordExtract.word_extract())
+if __name__ == '__main__':
+    test_word_file = WordExtractor('./上海应用技术大学关于开展2019年学习标兵、学习型寝室的评选通知.doc')
+    test_word_file.convert2png()
+    print(test_word_file.text())
 
-pdfExtract = PdfExtract()
-pdfExtract.load_path('./上海应用技术大学疫情防控指南.pdf')
-pdfExtract.convert2png('../CNN')
-print(pdfExtract.pdf_extract())
+    test_pdf_file = PdfExtractor('./上海应用技术大学疫情防控指南.pdf')
+    test_pdf_file.page2png()
+    print(test_pdf_file.text())
