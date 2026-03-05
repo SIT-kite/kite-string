bfa9720521f60387ae963fa2c7b91d5d6c277b21
sunnysab <i@sunnysab.cn>
2026-03-05 21:00:50 +0800

    chore(spider): update user agent to firefox 148

--- kite/settings.py
+++ kite/settings.py
@@ -31,8 +31,7 @@ SPIDER_MODULES = ['kite.spiders']
 NEWSPIDER_MODULE = 'kite.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 ' \
-             'Safari/537.36 Edg/87.0.664.75 '
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:148.0) Gecko/20100101 Firefox/148.0'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
